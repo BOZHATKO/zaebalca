03e61edc0af1bd693a954fe94944b756fc60f2ed
Alecsandr <79801722+SENIA918@users.noreply.github.com>
2023-01-25 11:45:33 +0300

    last 22

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+let openButton = document.querySelector('openButton');
+
+openButton.addEventListener("DOMContentLoaded", function() {
+  alert("DOM построен");
+});
+  // document.getElementById("popupForm").style.display = "block";
+
+// function closeForm() {
+//   document.getElementById("popupForm").style.display = "none";
+// }
