dc669d1e7dc77f238fc58ae4b2d63036256df5b0
Alecsandr <79801722+SENIA918@users.noreply.github.com>
2023-01-25 18:39:19 +0300

    video

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,26 @@
-let openButton = document.querySelector('openButton');
+const body = document.body;
+const btn = document.querySelector("#btn");
+const btnClose = document.querySelector("#btn_close");
+const modal = document.querySelector("#modal");
 
-openButton.addEventListener("DOMContentLoaded", function() {
-  alert("DOM построен");
+btn.addEventListener("click", btnHandler);
+btnClose.addEventListener("click", btnCloseHandler);
+
+function btnHandler(e) {
+  e.preventDefault();
+  body.classList.add("lock");
+  modal.classList.add("modal--open"); 
+}
+
+function btnCloseHandler(e) {
+  e.preventDefault();
+  body.classList.remove("lock"); 
+  modal.classList.remove("modal--open"); 
+}
+
+window.addEventListener("click", function (e) {
+  if (e.target == modal) {
+    body.classList.remove("lock");
+    modal.classList.remove("modal--open");
+  }
 });
-  // document.getElementById("popupForm").style.display = "block";
-
-// function closeForm() {
-//   document.getElementById("popupForm").style.display = "none";
-// }
