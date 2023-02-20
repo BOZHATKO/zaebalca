f3258819a9662fbc7b2869ed66334a1ef69e2a78
Alecsandr <79801722+SENIA918@users.noreply.github.com>
2023-02-20 17:29:57 +0300

    new app

---
+++ assets/js/invesr.en.js
@@ -0,0 +1,28 @@
+let readMore=()=> {
+	var dots=document.getElementById("dots");
+	var more=document.getElementById("more");
+	var btn=document.getElementById("btn");
+
+	if(dots.style.display==="none"){
+		dots.style.display="inline";
+		btn.innerHTML="Read more";
+		more.style.display="none";
+	}else {
+		dots.style.display="none";
+		btn.innerHTML="Hide";
+		more.style.display="inline";
+	}
+}
+
+function copyText(){
+	const text = document.querySelector(".card-body__list_2").innerText;
+	const elem = document.createElement('textarea')
+	elem.value = text;
+	elem.setAttribute('readonly', '');
+	elem.style.position = 'absolute';
+	elem.style.left = '-9999px';
+	document.body.appendChild(elem);
+	elem.select();
+	document.execCommand('copy');
+	document.body.removeChild(elem)
+}
---
+++ assets/js/main.js
@@ -0,0 +1,42 @@
+let videoPlayer = document.querySelector('.video-player')
+let videoContainer = document.querySelector('.video-container')
+let close = document.querySelector('.close')
+let video = document.querySelector('.video-container').querySelector('.video')
+videoPlayer.addEventListener('click', function (){
+  videoContainer.classList.add('show');
+  video.play()
+})
+
+close.addEventListener('click',function (){
+    videoContainer.classList.remove('show')
+    video.pause()
+})
+
+function copyText(){
+    const text = document.querySelector(".card-body__list_2").innerText;
+    const elem = document.createElement('textarea')
+    elem.value = text;
+    elem.setAttribute('readonly', '');
+    elem.style.position = 'absolute';
+    elem.style.left = '-9999px';
+    document.body.appendChild(elem);
+    elem.select();
+    document.execCommand('copy');
+    document.body.removeChild(elem)
+}
+function copyTextTwo(){
+    const textTwo = document.querySelector(".card-body__list_4").innerText;
+    const element = document.createElement('textarea')
+    element.value = textTwo;
+    element.setAttribute('readonly', '');
+    element.style.position = 'absolute';
+    element.style.left = '-9999px';
+    document.body.appendChild(element);
+    element.select();
+    document.execCommand('copy');
+    document.body.removeChild(element)
+}
+  const closeMenu = document.querySelector('.close_menu');
+   closeMenu.addEventListener('click', function (){
+   document.getElementById('myCheck').checked = false
+})
