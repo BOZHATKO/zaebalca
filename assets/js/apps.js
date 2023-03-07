948937d46c6bed1c168b10f2f5986f6f0c95f0f1
Alecsandr <79801722+SENIA918@users.noreply.github.com>
2023-03-07 09:54:17 +0300

    res 2560

--- assets/js/apps.js
+++ assets/js/apps.js
@@ -1,1 +1,40 @@
-const swiper=new Swiper(".swiper",{direction:"horizontal",loop:!0,slidesPerView:5,spaceBetween:10,autoplay:{delay:3e3},pagination:{el:".swiper-pagination",type:"bullets",clickable:!0},breakpoints:{320:{slidesPerView:2,spaceBetween:100},375:{slidesPerView:2,spaceBetween:20},640:{slidesPerView:3,spaceBetween:20},768:{slidesPerView:2.5,spaceBetween:10},1024:{slidesPerView:3,spaceBetween:10},1200:{slidesPerView:5,spaceBetween:10}}});
+const swiper = new Swiper(".swiper", {
+  direction: "horizontal",
+  loop: !0,
+  slidesPerView: 5,
+  spaceBetween: 10,
+  autoplay: {
+      delay: 3e3
+  },
+  pagination: {
+      el: ".swiper-pagination",
+      type: "bullets",
+      clickable: !0
+  },
+  breakpoints: {
+      320: {
+          slidesPerView: 2,
+          spaceBetween: 100
+      },
+      375: {
+          slidesPerView: 2,
+          spaceBetween: 20
+      },
+      640: {
+          slidesPerView: 3,
+          spaceBetween: 20
+      },
+      768: {
+          slidesPerView: 2.5,
+          spaceBetween: 10
+      },
+      1024: {
+          slidesPerView: 3,
+          spaceBetween: 10
+      },
+      1200: {
+          slidesPerView: 5,
+          spaceBetween: 10
+      }
+  }
+});
